27c363c74ba82ee39e49a6fd435b492dea552077
Neil Sekhon <nsekhon.96@gmail.com>
2018-06-24 16:39:05 -0500

    jjpjfi

---
+++ Watch Extension/TimePicker.swift
@@ -0,0 +1,55 @@
+//
+//  TimePicker.swift
+//  AirAber
+//
+//  Created by Neil Sekhon on 6/24/18.
+//  Copyright © 2018 Mic Pringle. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class TimePicker : WKInterfaceController {
+  
+    var minutes = Int()
+    var seconds = Int()
+  
+    @IBOutlet var minutePicker: WKInterfacePicker!
+    @IBOutlet var secondPicker: WKInterfacePicker!
+    
+    @IBOutlet var setButton: WKInterfaceButton!
+    
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+    
+        var items = [WKPickerItem]()
+        for i in 0...12 {
+            items.append(makePickerItem(num: i))
+        }
+        
+        minutePicker.setItems(items)
+        
+        items.removeAll()
+        for i in 0...10 {
+            items.append(makePickerItem(num: i))
+        }
+        
+        secondPicker.setItems(items)
+    }
+    
+    func makePickerItem(num: Int) -> WKPickerItem {
+        let item = WKPickerItem()
+        item.title = String(num)
+        
+        return item
+    }
+    
+    @IBAction func setPressed() {
+        self.dismiss()
+    }
+    
+    @IBAction func pickerChanged(_ value: Int) {
+    }
+    
+}
