753d197e31785d834d33d527de67c8756b91044d
Neil Sekhon <nsekhon.96@gmail.com>
2018-06-24 15:54:05 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#  AirAber
+
+## watchOS 4 Tutorial Part 1 Starter
+
+This is the iOS app: it doesn't do anything.
+
+You'll add a Watch app to list information about Air Aber flights, and let the user check in.
+
+The Shared group contains files that the Watch app will use.
+
+In Part 1, you'll add the WatchKit app, and create the Flight interface.
---
+++ Watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Watch Extension
+//
+//  Created by Neil Sekhon on 6/24/18.
+//  Copyright © 2018 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
